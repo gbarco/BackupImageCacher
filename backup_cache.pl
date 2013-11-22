961f49afe41784c25b356567ac809eda6b8580b8
Gonzalo Barco <gbarcouy@gmail.com>
2013-11-22 12:14:18 -0200

    Functionality migrated to module.

--- backup_cache.pl
+++ backup_cache.pl
@@ -1,30 +1,54 @@
 use strict;
+use warnings;
 use Config::Simple;
 use Getopt::Long;
+use DateTime;
+use Local::AWS::Credentials;
+use HomeCo::AWS::BackupImageCacher;
 
-#calculate config file name
+# calculate config file name
 my $config = {}; #must be empty hash for Config::Simple
 my $config_file = $0; $config_file =~ s/\.([^\.]+)$/\.cfg/;
 die("No config file $config_file!") unless -f $config_file;
 
-#read config file
-Config::Simple->import_from( $config_file, $config);
-die("No BaseThumbs defined") unless defined $config->{BaseThumbs};
-die("BaseThumbs does not exist") unless -d $config->{BaseThumbs};
-die("No BaseImageCache defined") unless dfined  $config->{BaseImageCache};
-die("BaseImageCache does not exist") unless -d $config->{BaseImageCache};
-die("VaultRegion does not exist. Set an AWS Region") unless -d $config->{VaultRegion};
-die("VaultName does not exist. Set an existing AWS Glacier Vault") unless -d $config->{VaultRegion};
+# default values
+$config->{Daily} = '';
+$config->{Monthly} = '';
+$config->{Date} = DateTime->now->ymd;
 
-my $archive_filename_map = 'archive_filename'; #default archive to filename map file
-my $credential_file_path = '.aws_credentials.txt'; #default credential file
-my $region = $config->{VaultRegion}; #default region
-my $vault = $config->{VaultName}; #no default vault
-my $result = GetOptions(
-	"archivefilenamemap=s" => \$archive_filename_map,
-	"vault=s" => \$vault,
-	"region=s" => \$region,
-	"credentials=s" => \$credential_file_path,
+# read config file
+Config::Simple->import_from( $config_file, $config);
+
+# merge commandl line options with config file
+die("Error reading command line options.") unless GetOptions(
+	"vault=s" => $config->{VaultName},
+	"region=s" => $config->{VaultRegion},
+	"credentials=s" => $config->{AWSCredentials},
+	"daily" => $config->{Daily},
+	"monthly" => $config->{Monthly},
+	"date=s" => $config->{Date},
 );
+
+# read credentials
+( $config->{AWSAccessKey}, $config->{AWSSecret} ) = AWS::Local::Credentials::read_aws_credentials( $config->{AWSCredentials} );
+
+#request parameter check
+eval {
+	HomeCo::AWS::BackupImageCacher::check_parameters( $config );
+};
+if ( $@ ) {
+	die( $@ );
+}
+
+eval {
+	HomeCo::AWS::BackupImageCacher::backup( $config );
+};
+if ( $@ ) {
+	die( $@ );
+}
+
+
+
+
 
 
