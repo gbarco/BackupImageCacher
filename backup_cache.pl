37110eb672df1493b87374d832ddd60dcae9e943
Gonzalo Barco <gonzalo@computadorasyservice.com>
2014-05-25 22:02:57 -0300

    Verbose logging. Extra error catching.

--- backup_cache.pl
+++ backup_cache.pl
@@ -6,34 +6,47 @@ use DateTime;
 use Local::AWS::Credentials;
 use HomeCo::AWS::BackupImageCacher;
 
-# calculate config file name
+# Calculate config file name
 my $config = {}; #must be empty hash for Config::Simple
 my $config_file = $0; $config_file =~ s/\.([^\.]+)$/\.conf/;
-die("No config file $config_file!") unless -f $config_file;
 
-# default values
+# Check we have a config file with the expected name
+HomeCo::AWS::BackupImageCacher::_logdie( 'ERROR', "No config file $config_file!" ) unless -f $config_file;
+
+# Default values
 $config->{Daily} = '';
 $config->{Monthly} = '';
-$config->{Date} = DateTime->now->ymd('');
+$config->{Date} = DateTime->now->set_time_zone('local')->subtract( days => 1 )->ymd('');
 
-# read config file
-Config::Simple->import_from( $config_file, $config);
+# Read config file
+Config::Simple->import_from( $config_file, $config );
 
-# merge command line options with config file, command line has precedense
-die("Error reading command line options.") unless GetOptions(
-	HomeCo::AWS::BackupImageCacher::parameter_match( $config ),
+# Merge command line options with config file, command line has precedense
+HomeCo::AWS::BackupImageCacher::_logdie( 'ERROR', "Failed reading command line parameters." )
+	unless GetOptions(
+		HomeCo::AWS::BackupImageCacher::parameter_match( $config ),
 );
 
-# read credentials
-( $config->{AWSAccessKey}, $config->{AWSSecret} ) = Local::AWS::Credentials::read_aws_credentials( $config->{AWSCredentials} );
+eval {
+	# Read credentials
+	( $config->{AWSAccessKey}, $config->{AWSSecret} ) = Local::AWS::Credentials::read_aws_credentials( $config->{AWSCredentials} );
+};
+if ( $@ ) {
+	HomeCo::AWS::BackupImageCacher::_logdie( 'ERROR', "Failed reading credentials with error: $@." );
+}
 
-#request parameter check
+# Request parameter check
 eval {
 	HomeCo::AWS::BackupImageCacher::check_parameters( $config );
+};
+if ( $@ ) {
+	HomeCo::AWS::BackupImageCacher::_logdie( 'ERROR', "Parameter check failed with error: $@." );
+}
+eval {
 	HomeCo::AWS::BackupImageCacher::open_metadata_store( $config );
 };
 if ( $@ ) {
-	die( $@ );
+	HomeCo::AWS::BackupImageCacher::_logdie( 'ERROR', "Opening metadata store failed with error: $@." );
 }
 
 if ( $config->{Daily} || $config->{Monthly} ) {
@@ -41,17 +54,22 @@ if ( $config->{Daily} || $config->{Monthly} ) {
 		HomeCo::AWS::BackupImageCacher::backup( $config );
 	};
 	if ( $@ ) {
-		die( $@ );
+		HomeCo::AWS::BackupImageCacher::_logdie( 'ERROR', "Backup failed with error: $@." );
 	}
 } elsif ( $config->{Cleanup} ) {
-	HomeCo::AWS::BackupImageCacher::cleanup( $config );
+	eval {
+		HomeCo::AWS::BackupImageCacher::cleanup( $config );	
+	};
+	if ( $@ ) {
+		HomeCo::AWS::BackupImageCacher::_logdie( 'ERROR', "Cleanup failed with error: $@." );
+	}
 } else {
-	die("Inconsistent parameters. Daily, Montlhy or Cleanup must be specified.");
+	HomeCo::AWS::BackupImageCacher::_logdie( 'ERROR', "Inconsistent parameters. Daily, Monthly or Cleanup must be specified." );
 }
 
 eval {
 	HomeCo::AWS::BackupImageCacher::close_metadata_store( $config );
 };
 if ( $@ ) {
-	die( $@ );
+	HomeCo::AWS::BackupImageCacher::_log( 'INFO', "Could not close metadata store with error: $@" );
 }
